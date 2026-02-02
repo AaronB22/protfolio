6bd249ea372e40f13a35516337ea7024fb0bd7a1
AaronB22 <aaronthesnowboarder@live.com>
2026-02-01 22:32:11 -0800

    express setup

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+
+const PORT=3000;
+
+app.use(express.static('public'))
+
+app.get('/', (req,res)=>{
+    res.sendFile(`${import.meta.dirname}/views/index.html`)
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Listening on http://localhost${PORT}`)
+})

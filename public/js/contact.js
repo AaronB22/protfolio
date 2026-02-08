417f6ebad10ae3e2ed16700f16a1b3a7a1725ccc
Aaron Bailey <aaronthesnowboarder@live.com>
2026-02-08 11:55:41 -0800

    added validater

---
+++ public/js/contact.js
@@ -0,0 +1,39 @@
+document.getElementById("contact-form").onsubmit = validate;
+
+function validate() {
+
+    let isValid = true;
+    let fname = document.getElementById("fname").value.trim();
+    let lname = document.getElementById("lname").value.trim();
+    let email = document.getElementById("email").value.trim();
+    let linkedIn = document.getElementById("link").value.trim();
+    let meet = document.getElementById('meet');
+    let mail=document.getElementById('mail');
+
+    if (!fname) {
+        isValid = false
+        document.getElementById("lerr").style.display = "block"
+    }
+    if (!lname) {
+        isValid = false
+        document.getElementById("ferr").style.display = "block"
+    }
+    if (email && !email.includes("@")) {
+        isValid = false
+        document.getElementById("emerr").style.display = "block"
+    }
+    if (linkedIn && !linkedIn.includes("https://linkedin.com/in/")) {
+        isValid = false
+        document.getElementById("lkerr").style.display = "block"
+    }
+    if (meet.value === "none") {
+        isValid = false
+        document.getElementById("merr").style.display = "block"
+    }
+    if(mail.checked && !email){
+                isValid = false
+        document.getElementById("emerr").style.display = "block"
+    }
+
+    return isValid;
+}
